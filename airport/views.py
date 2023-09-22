db3572831f689cdc27406bf75072051b216e3d13
chriford-siame <chrifordsiame@gmail.com>
2023-09-22 08:06:17 +0200

    modify urls

--- airport/views.py
+++ airport/views.py
@@ -4,16 +4,17 @@ import requests
 import random
 import environ
 from rest_framework.response import Response
-
+from django.contrib import messages
 # env = environ.Env()
 # environ.Env.read_env()
 
-def process_payment():
-    auth_token = 'FLWSECK_TEST-46ebaa390394ca4757db93674275792a-X'
+
+def process_payment(plan, amount):
+    auth_token = 'FLWSECK_TEST-a987ec54fe7b47dfa301709a3a4096d5-X'
     hed = {'Authorization': 'Bearer ' + auth_token}
     data = {
         "tx_ref": ''+str(math.floor(1000000 + random.random()*9000000)),
-        "amount": 2100,
+        "amount": amount,
         "currency": "ZMW",
         "redirect_url": "http://localhost:8001/payment/success",
         "payment_options": "card, ZBMobile",
@@ -27,49 +28,118 @@ def process_payment():
             "name": 'Chriford Siame'
         },
         "customizations": {
-            "title": "Parking Slot",
-            "description": "",
+            "title": f"Plan - {plan.title()}",
+            "description": "AirportZ payment form",
             "logo": "https://www.moderncremation.ca/wp-content/uploads/PaymentGateway.jpg"
         }
     }
     url = ' https://api.flutterwave.com/v3/payments'
     response = requests.post(url, json=data, headers=hed)
     response = response.json()
-    link = response['data']['link']
-    return link
+    print(response)
+    return response['data']['link']
 
 
 def index(request):
-    return render(request, 'other/index.html')
+    context = {
+        'airports': [
+            {
+
+                'index': '01',
+                'name': 'Keneth Kauda',
+            },
+            {
+                'index': '02',
+                'name': 'Kabwe',
+            },
+            {
+                'index': '03',
+                'name': 'CIP',
+            },
+            {
+                'index': '04',
+                'name': 'Chipata',
+            },
+            {
+                'index': '05',
+                'name': 'Solwezi',
+            },
+            {
+                'index': '06',
+                'name': 'Mfuwe',
+            },
+            {
+                'index': '07',
+                'name': 'Livingstone',
+            },
+        ]
+    }
+    
+    return render(request, 'other/index.html', context)
+
 
 def about(request):
     return render(request, 'other/about.html')
 
+
 def contact(request):
     return render(request, 'other/contact.html')
 
+
 def blogs(request):
-    return render(request, 'other/airport-grid.html')
+    return render(request, 'other/blogs.html')
+
 
 def blog(request):
-    return render(request, 'other/blog-single.html')
+    return render(request, 'other/blog.html')
+
 
 def payment_processor(request):
-    return redirect(str(process_payment()))
+    payment_form_url = str(process_payment())
+    return redirect(payment_form_url)
 
-def payment_fail(request):
-    return render(request, 'payment/success.html')
 
-def parking_slot_validity(request):
+def parking_slot_view(request, airport):
+    message = ''
     if request.method == 'POST':
         entry_date = request.POST.get('entry-date')
         entry_time = request.POST.get('entry-time')
-        return
+        if entry_date == '2023-09-22':
+            message = 'The parking slot at the selected entry date has been occupied, enter another one.'
+        else:
+            return redirect('slot_plans')
+
+    return render(request, 'other/slot-view.html', {'airport': airport})
+    # return render(request, 'temp/property-single.html', {'airport': airport, 'message': message})
+
 
 def parking_slot_plans(request):
+    if request.method == 'POST':
+        plan = request.POST.get('plan', None)
+        if plan:
+            amount = request.POST.get('amount', 0)
+            if amount == 0 and isinstance(amount, int):
+                messages.add_message(
+                    request, messages.ERROR, 'An error occurred while, contact support for help.')
+                return redirect('index')
+            obj = dict(
+                plan=plan,
+                amount=int(amount),
+            )
+            # TODO: query the plan dynamically by submitted plan
+            return redirect(
+                str(process_payment(obj['plan'], obj['amount']))
+            )
+        else:
+            return redirect('index')
     return render(request, 'other/plans.html')
 
+
 def payment_success(request):
-    status=request.GET.get('status', None)
-    tx_ref=request.GET.get('tx_ref', None)
+    status = request.GET.get('status', None)
+    tx_ref = request.GET.get('tx_ref', None)
+    return render(request, 'payment/success.html')
+
+
+def payment_fail(request):
     return render(request, 'payment/fail.html')
